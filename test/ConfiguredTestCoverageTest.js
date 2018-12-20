721656ab6beb7300fd737a0f6ffdc263319f0d60
gismayilov <guseynism@gmail.com>
2018-12-21 00:34:29 +0300

    refactor

--- test/ConfiguredTestCoverageTest.js
+++ test/ConfiguredTestCoverageTest.js
@@ -2,9 +2,9 @@ const { DeepEqualAssertion } = require('@cuties/assert')
 const { ConfiguredTestCoverage, ExecutedTestCoverage } = require('./../index')
 
 new DeepEqualAssertion(
-  new ConfiguredTestCoverage(
-    new ExecutedTestCoverage(
-      process, './fake/test/script-test'
-    ), { 'lines': 100, 'functions': 100, 'branches': 100 }
+  new ExecutedTestCoverage(
+    new ConfiguredTestCoverage(process, {
+      'lines': 100, 'functions': 100, 'branches': 100
+    }), './fake/test/script-test'
   ), process
 ).call()
