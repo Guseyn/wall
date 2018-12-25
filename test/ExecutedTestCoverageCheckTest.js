3cc78e44a0fb7f906bd9f1610a0ccc1904357c64
gismayilov <guseynism@gmail.com>
2018-12-25 13:17:44 +0300

    fixed test

--- test/ExecutedTestCoverageCheckTest.js
+++ test/ExecutedTestCoverageCheckTest.js
@@ -2,9 +2,9 @@ const { DeepEqualAssertion } = require('@cuties/assert')
 const { ExecutedTestCoverageCheck, ExecutedTestCoverage } = require('./../index')
 
 new DeepEqualAssertion(
-  new ExecutedTestCoverage(
-    new ExecutedTestCoverageCheck(process, {
+  new ExecutedTestCoverageCheck(
+    new ExecutedTestCoverage(process, './fake/test/script-test'), {
       'lines': 100, 'functions': 100, 'branches': 100
-    }), './fake/test/script-test'
+    }
   ), process
 ).call()
